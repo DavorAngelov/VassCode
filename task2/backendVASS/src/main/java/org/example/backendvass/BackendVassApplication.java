f87674cea6c3aadddbd3bc39156498a94c65d89f
davorangelov <davor.angelov@students.finki.ukim.mk>
2026-05-15 16:12:34 +0200

    adding backend

---
+++ task2/backendVASS/src/main/java/org/example/backendvass/BackendVassApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendvass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendVassApplication.class, args);
+    }
+
+}
---
+++ task2/backendVASS/src/test/java/org/example/backendvass/BackendVassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendvass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
