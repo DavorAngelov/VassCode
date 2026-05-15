51459e9afe236504dc23cd0d99a4d639e3b639a3
davorangelov <davor.angelov@students.finki.ukim.mk>
2026-05-15 17:17:42 +0200

    added frontend fetch

--- task2/backendVASS/src/main/resources/test.sql
+++ task2/backendVASS/src/main/resources/test.sql
@@ -1,6 +1,15 @@
-INSERT INTO employees(name, role, department, email, phone, image, online)
+INSERT INTO employees(id,name, role, department, email,image)
 VALUES
-    ('da','Senior','engineer',
-     'dad','11111',
-     'https://randomuser.me/api/portraits/men/75.jpg',1),
+    (1,'Davor','engineer',
+     'department1','dad@gmail.com',
+     'https://randomuser.me/api/portraits/men/75.jpg'),
+
+    (2,'Ama','engineer',
+     'department2','dad@gmail.com',
+     'https://randomuser.me/api/portraits/men/75.jpg'),
+
+    (3,'david','engineer',
+     'department3','dadaa@gmail.com',
+     'https://randomuser.me/api/portraits/men/75.jpg')
+    ;
 
